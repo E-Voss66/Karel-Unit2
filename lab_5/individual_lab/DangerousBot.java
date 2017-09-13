c5c607ef91368b56aeed1fc14fbb3ef69c592cef
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-09-13 10:03:08 -0700

    Lab 5 Problem 1

--- lab_5/individual_lab/DangerousBot.java
+++ lab_5/individual_lab/DangerousBot.java
@@ -7,13 +7,49 @@ import kareltherobot.*;
  */
 public class DangerousBot extends Robot
 {
+    int beeperCount = 0;
     public DangerousBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void choosePile() {
-        
+        pickUpBeepers();
+        if(pileIsOdd()){
+            faceWest();
+            move();
+        }
+        else {
+            faceEast();
+            move();
+        }
     }
-   
+    
+    public void faceWest() {
+        while(!facingWest()) {
+            turnLeft();
+        }
+    }
+    
+    public void faceEast() {
+        while(!facingEast()) {
+            turnLeft();
+        }
+    }
+    
+    public void pickUpBeepers(){
+        while(nextToABeeper()){
+            pickBeeper();
+            beeperCount++;
+        }
+    }
+    
+    public boolean pileIsOdd() {
+        if(beeperCount % 2 == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 }
 
