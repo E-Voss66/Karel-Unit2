003bca8386aa72d8e520d0a7d76608734a17722b
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-09-15 08:53:14 -0700

    Lab 5 Problem 3

--- lab_5/individual_lab/DemocracyBot.java
+++ lab_5/individual_lab/DemocracyBot.java
@@ -7,12 +7,68 @@ import kareltherobot.*;
  */
 public class DemocracyBot extends Robot
 {
+    int moveCount = 0;
     public DemocracyBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void fixBallots() {
-        
+        while(moveCount < 7) {
+            findBeeper();
+            if(moveCount < 7){
+                clearTopRoom();
+                clearBottomRoom();
+            }
+        }
+    }
+    
+    public void faceNorth(){
+        while(!facingNorth()) {
+            turnLeft();
+        }
+    }
+    
+    public void faceSouth(){
+        while(!facingSouth()) {
+            turnLeft();
+        }
+    }
+    
+    public void faceEast(){
+        while(!facingEast()) {
+            turnLeft();
+        }
+    }
+    
+    public void faceWest(){
+        while(!facingWest()) {
+            turnLeft();
+        }
+    }
+    
+    public void findBeeper(){
+        while(!nextToABeeper() && moveCount < 7){
+            move();
+            moveCount++;
+        }
+    }
+    
+    public void clearTopRoom(){
+        faceNorth();
+        move();
+        pickBeeper();
+        faceSouth();
+        move();
+    }
+    
+    public void clearBottomRoom(){
+        faceSouth();
+        move();
+        pickBeeper();
+        faceNorth();
+        move();
+        faceEast();
+        move();
     }
    
 }
